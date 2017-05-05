c251ac264c8e57e53d17cde32b31b05902511cb2
luixlacrux <luixlacrux18@gmail.com>
2017-05-05 00:43:02 -0400

    :sparkles: [Add] Basic TO-Do app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-ReactDOM.render(
-  <App />,
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
+
+
+export default App
