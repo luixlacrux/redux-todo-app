d045aa2b2e7250baf20e46348bb95fce3f49be5a
luixlacrux <luixlacrux18@gmail.com>
2017-05-10 23:40:25 -0400

    :poop: [Update] usage the prop-types package from npm instead

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import '../styles/components/Link.css'
 
 const Link = ({ active, color, children, onClick }) => {
--- src/localStorage.js
+++ src/localStorage.js
@@ -13,7 +13,6 @@ export const loadState = () => {
 export const saveState = (state) => {
   try {
     const serializedState = JSON.stringify(state)
-    debugger
     localStorage.setItem('state', serializedState)
   } catch (err) {
     console.error(err)
