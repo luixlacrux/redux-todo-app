d44081742b10cb259b0502ac3d555a38cd2bc43b
luixlacrux <luixlacrux18@gmail.com>
2017-05-10 23:14:11 -0400

    :sparkles: [Add] persisting the state to the localStorage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
-let nextTodoId = 0
+import { v4 } from 'node-uuid'
+
 export const addTodo = (text) => {
   return {
     type: 'ADD_TODO',
-    id: nextTodoId++,
+    id: v4(),
     text,
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import throttle from 'lodash/throttle'
 import todoApp from './reducers'
 import App from './components/App'
+import { loadState, saveState } from './localStorage'
 import './styles/index.css'
+
+const initialState = loadState()
 
 let store = process.env.NODE_ENV !== 'production'
   ? createStore(
-    todoApp, {},
+    todoApp,
+    initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ) : createStore(todoApp)
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  })
+}, 1000))
 
 render(
   <Provider store={store}>
