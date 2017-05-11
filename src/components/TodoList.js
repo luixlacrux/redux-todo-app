c41ecf7137687403c35d49cb79a2273d739c4819
luixlacrux <luixlacrux18@gmail.com>
2017-05-10 20:57:43 -0400

    :sparkles: [Add] animations to TodoItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,24 @@
 import React, { PropTypes } from 'react'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 import Todo from './Todo'
 
 const TodoList = ({ todos, onTodoClick, onDeleteTodo }) => (
   <ul style={{
     display: todos.length ? 'block' : 'none'
   }}>
-    {todos.map(todo =>
-      <Todo
-        key={todo.id}
-        {...todo}
-        onClick={() => onTodoClick(todo.id)}
-        onDelete={() => onDeleteTodo(todo.id)}
-      />
-    )}
+    <CSSTransitionGroup
+      transitionName="Todo"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={300}>
+      {todos.map(todo =>
+        <Todo
+          key={todo.id}
+          {...todo}
+          onClick={() => onTodoClick(todo.id)}
+          onDelete={() => onDeleteTodo(todo.id)}
+        />
+      )}
+    </CSSTransitionGroup>
   </ul>
 )
 
